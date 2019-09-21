02cc44385f8eba5694d558bba0d9c0a1ea97e843
shy <szymon.lost@gmail.com>
2019-09-21 17:22:38 +0200

    Added Scripts and Game Scene

---
+++ Tetricar/Assets/Scripts/Car.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private Transform t;
+    private Rigidbody rb;
+
+    public float speed;
+    public float rotationDuration;
+    private bool isRotating = false;
+    public AnimationCurve rotationCurve;
+    private float angleOfCar;
+    private Vector3 scale = Vector3.zero;
+
+    public bool gameOver = false;
+    public Camera camera;
+    int i = 0;
+    void Start()
+    {
+        t = transform;
+        rb = GetComponent<Rigidbody>();
+        speed = 5f;
+        rotationDuration = 0.2f;
+        scale = t.localScale;
+        
+    }
+
+    void Update()
+    {
+        t.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (!gameOver && Input.GetKeyDown(KeyCode.A) && !isRotating && angleOfCar > -90f)
+        {
+            StartCoroutine(Rotate(Vector3.up, -90f));
+        }
+        if (!gameOver && Input.GetKeyDown(KeyCode.D) && !isRotating && angleOfCar < 90f)
+        {
+            StartCoroutine(Rotate(Vector3.up, 90f) );
+        }
+
+        if (!gameOver && !Physics.Raycast(t.position, Vector3.down, 5f))
+        {
+            gameOver = true;
+            GameOver();
+        }
+    }
+
+    private IEnumerator Rotate(Vector3 rotationVector, float rotationAngle)     //rotate Right Left
+    {
+        angleOfCar += rotationAngle;
+        isRotating = true;
+        float rotationTimer = 0f;
+        float rotatedAngle = 0f;
+        float oldAngle = 0f;
+        float newAngle = 0f;
+        float angle = 0f;
+
+        while (rotationTimer <= rotationDuration)
+        {
+            float rotationPercentage = rotationTimer / rotationDuration;
+            newAngle = rotationCurve.Evaluate(rotationPercentage) * rotationAngle;
+            angle = newAngle - oldAngle;
+
+            t.Rotate(rotationVector * angle, Space.World);
+
+            rotatedAngle += angle;
+
+            rotationTimer += Time.fixedDeltaTime;
+            oldAngle = newAngle;
+            yield return null;
+        }
+        t.Rotate(rotationVector * (rotationAngle - rotatedAngle), Space.World);
+        //yield return new WaitForSeconds(0.1f);
+        isRotating = false;
+    }
+
+    private void GameOver()
+    {
+        camera.transform.parent = null;
+        rb.constraints = 0;
+        Debug.Log("GameOver");
+    }
+}
---
+++ Tetricar/Assets/Scripts/TetrisBlock.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisBlock : MonoBehaviour
+{
+    private Transform t;
+    private Rigidbody rb;
+
+    // Rotation
+    public float rotationDuration;
+    private bool isRotating = false;
+    public AnimationCurve rotationCurve;
+
+    // Moving
+    public float movingDuration;
+    private bool isMoving;
+    public AnimationCurve movingCurve;
+
+    private float sizeOfBlock = 10f;
+    public float forceValue = 0f;
+
+    void Start()
+    {
+        t = transform;
+        rb = GetComponent<Rigidbody>();
+
+        rotationDuration = 0.15f;
+        movingDuration = 0.2f;
+        forceValue = 100f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            StartCoroutine(Rotate(Vector3.up, 90f));
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            StartCoroutine(Move(Vector3.left));
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            StartCoroutine(Move(Vector3.right));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Drop();
+        }
+
+    }
+
+    private IEnumerator Rotate(Vector3 rotationVector, float rotationAngle)     //rotate Right Left
+    {
+        isRotating = true;
+        float rotationTimer = 0f;
+        float rotatedAngle = 0f;
+        float oldAngle = 0f;
+        float newAngle = 0f;
+        float angle = 0f;
+
+        while (rotationTimer <= rotationDuration)
+        {
+            float rotationPercentage = rotationTimer / rotationDuration;
+            newAngle = rotationCurve.Evaluate(rotationPercentage) * rotationAngle;
+            angle = newAngle - oldAngle;
+
+            t.Rotate(rotationVector * angle, Space.World);
+
+            rotatedAngle += angle;
+
+            rotationTimer += Time.fixedDeltaTime;
+            oldAngle = newAngle;
+            yield return null;
+        }
+        t.Rotate(rotationVector * (rotationAngle - rotatedAngle), Space.World);
+        //yield return new WaitForSeconds(0.1f);
+        isRotating = false;
+    }
+
+    private IEnumerator Move(Vector3 direction)
+    {
+        isMoving = true;
+        float movingTimer = 0f;
+        Vector3 startPosition = t.position;
+
+        while (movingTimer < movingDuration)
+        {
+            float movingPercentage = movingTimer / movingDuration;
+            //t.Translate(direction * movingCurve.Evaluate(movingPercentage) * sizeOfBlock);
+            t.position = startPosition + (direction * movingCurve.Evaluate(movingPercentage) * sizeOfBlock);
+            movingTimer += Time.deltaTime;
+            yield return null;
+        }
+        t.position = startPosition + direction * sizeOfBlock;
+        isMoving = false;
+    }
+
+    private void Drop()
+    {
+        rb.AddForce(Vector3.back * forceValue, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Tetris"))
+        {
+            Destroy(rb, 1f);
+            Destroy(GetComponent<TetrisBlock>());
+        }
+    }
+
+
+}
